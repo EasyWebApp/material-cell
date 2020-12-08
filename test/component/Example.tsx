13873a9dbda99928754041cacb248b28c1b26903
TechQuery <shiy007@qq.com>
2020-12-08 18:22:53 +0800

    [remove] migrate Document site to an independent project

--- test/component/Example.tsx
+++
@@ -1,5 +0,0 @@
-import { WebCellProps, createCell } from 'web-cell';
-
-export function Example({ defaultSlot }: WebCellProps) {
-    return <div className="border border-light p-4">{defaultSlot}</div>;
-}
--- test/index.tsx
+++
@@ -1,5 +0,0 @@
-import { documentReady, render, createCell } from 'web-cell';
-
-import { PageRouter } from './page';
-
-documentReady.then(() => render(<PageRouter />));

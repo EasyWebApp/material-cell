3a475d67e9916ee6bc98582409620257d625e5b9
TechQuery <shiy2008@gmail.com>
2024-02-10 03:49:45 +0800

    [refactor] upgrade to WebCell 3, Material Design Web 1, Parcel 2 & Husky 9 [migrate] replace NPM with PNPM to reduce Disk Usage

--- ReadMe.md
+++ ReadMe.md
@@ -2,8 +2,8 @@
 
 [Material Web Components][1]'s [JSX Type definition][2] for [WebCell][3] engine
 
-[![NPM Dependency](https://david-dm.org/EasyWebApp/material-cell.svg)][4]
-[![CI & CD](https://github.com/EasyWebApp/material-cell/workflows/CI%20&%20CD/badge.svg)][5]
+[![NPM Dependency](https://img.shields.io/librariesio/github/EasyWebApp/material-cell.svg)][4]
+[![CI & CD](https://github.com/EasyWebApp/material-cell/actions/workflows/main.yml/badge.svg)][5]
 
 [![Open in Visual Studio Code](https://open.vscode.dev/badges/open-in-vscode.svg)][6]
 
@@ -15,17 +15,17 @@ https://material.web-cell.dev/
 
 ## Usage
 
-```Shell
+```shell
 npm install material-cell web-cell
 ```
 
 ### Example
 
-```Shell
+```shell
 npm install @material/mwc-icon
 ```
 
-```TSX
+```tsx
 import { render, createCell } from 'web-cell';
 import type {} from 'material-cell';
 import '@material/mwc-icon';
@@ -36,7 +36,7 @@ render(<mwc-icon>code</mwc-icon>);
 [1]: https://github.com/material-components/material-components-web-components
 [2]: https://www.typescriptlang.org/docs/handbook/jsx.html
 [3]: https://web-cell.dev/
-[4]: https://david-dm.org/EasyWebApp/material-cell
-[5]: https://github.com/EasyWebApp/material-cell/actions
+[4]: https://libraries.io/npm/material-cell
+[5]: https://github.com/EasyWebApp/material-cell/actions/workflows/main.yml
 [6]: https://open.vscode.dev/EasyWebApp/material-cell
 [7]: https://nodei.co/npm/material-cell/
--- test/index.tsx
+++ test/index.tsx
@@ -1,66 +1,42 @@
-import { documentReady, render, createCell } from 'web-cell';
-import '@material/mwc-icon';
-import '@material/mwc-icon-button';
-import '@material/mwc-icon-button-toggle';
-import '@material/mwc-button';
-import '@material/mwc-fab';
-import '@material/mwc-radio';
-import '@material/mwc-checkbox';
-import '@material/mwc-switch';
-import '@material/mwc-formfield';
-import '@material/mwc-textfield';
-import '@material/mwc-textarea';
-import '@material/mwc-list';
-import '@material/mwc-select';
-import '@material/mwc-menu';
-import type { Menu } from '@material/mwc-menu';
-import '@material/mwc-slider';
-import '@material/mwc-linear-progress';
-import '@material/mwc-circular-progress';
-import '@material/mwc-circular-progress-four-color';
-import '@material/mwc-tab';
-import '@material/mwc-tab-bar';
-import '@material/mwc-snackbar';
-import type { Snackbar } from '@material/mwc-snackbar';
-import '@material/mwc-dialog';
-import type { Dialog } from '@material/mwc-dialog';
-import '@material/mwc-top-app-bar';
-import '@material/mwc-top-app-bar-fixed';
-import '@material/mwc-drawer';
+import { DOMRenderer } from 'dom-renderer';
+import { documentReady } from 'web-utility';
 
-import type {} from '../source';
+import '@material/web/all';
+import { MdDialog } from '@material/web/all';
 
 import * as list from './list';
 
 document.addEventListener(
     'MDCTopAppBar:nav',
     ({ target }: Event) =>
-        ((target as HTMLElement).closest('mwc-drawer').open = true)
+        ((target as HTMLElement).closest('md-drawer').open = true)
 );
 
 documentReady.then(() =>
-    render(
-        <mwc-drawer hasHeader type="modal">
+    new DOMRenderer().render(
+        <md-drawer hasHeader type="modal">
             <span slot="title">Components</span>
             <span slot="subtitle">list</span>
-            <mwc-list>
+            <md-list>
                 {Object.entries(list).map(([id, title]) => (
-                    <mwc-list-item>
+                    <md-list-item>
                         <a
                             href={`#${id}`}
                             onClick={({ target }) =>
                                 ((target as HTMLElement).closest(
-                                    'mwc-drawer'
+                                    'md-drawer'
                                 ).open = false)
                             }
                         >
                             {title}
                         </a>
-                    </mwc-list-item>
+                    </md-list-item>
                 ))}
-            </mwc-list>
-            <mwc-top-app-bar-fixed slot="appContent">
-                <mwc-icon-button icon="menu" slot="navigationIcon" />
+            </md-list>
+            <md-top-app-bar-fixed slot="appContent">
+                <md-icon-button slot="navigationIcon">
+                    <md-icon>menu</md-icon>
+                </md-icon-button>
                 <div slot="title">Material Web Components</div>
                 <a
                     slot="actionItems"
@@ -68,137 +44,148 @@ documentReady.then(() =>
                     title="Source code"
                     href="https://github.com/EasyWebApp/material-cell"
                 >
-                    <mwc-icon>code</mwc-icon>
+                    <md-icon>code</md-icon>
                 </a>
                 <main>
                     <h2 id="icon">Icon</h2>
-                    <mwc-icon>code</mwc-icon>
+                    <md-icon>code</md-icon>
                     <h2 id="icon_button">Icon Button</h2>
-                    <mwc-icon-button icon="code" />
+                    <md-icon-button>
+                        <md-icon>code</md-icon>
+                    </md-icon-button>
                     <h2 id="icon_button_toggle">Icon Button Toggle</h2>
-                    <mwc-icon-button-toggle
-                        onIcon="star"
-                        offIcon="star_border"
-                    />
+                    <md-icon-button toggle>
+                        <md-icon>star</md-icon>
+                        <md-icon slot="selected">star_border</md-icon>
+                    </md-icon-button>
                     <h2 id="button">Button</h2>
-                    <mwc-button raised icon="code">
+                    <md-filled-button>
+                        <md-icon slot="icon">code</md-icon>
                         test
-                    </mwc-button>
+                    </md-filled-button>
                     <h2 id="fab">Fab</h2>
-                    <mwc-fab
-                        extended
-                        icon="shopping_cart"
-                        label="Add to cart"
-                    />
+                    <md-fab label="Add to cart">
+                        <md-icon slot="icon">shopping_cart</md-icon>
+                    </md-fab>
                     <h2 id="radio">Radio</h2>
-                    <mwc-radio name="group" />
-                    <mwc-radio name="group" checked />
+                    <md-radio name="group" />
+                    <md-radio name="group" checked />
                     <h2 id="checkbox">Checkbox</h2>
-                    <mwc-checkbox indeterminate />
+                    <md-checkbox indeterminate />
                     <h2 id="switch">Switch</h2>
-                    <mwc-switch />
+                    <md-switch />
                     <h2 id="form_field">Form Field</h2>
-                    <mwc-formfield label="check">
-                        <mwc-checkbox />
-                    </mwc-formfield>
+                    <label>
+                        <md-checkbox />
+                        check
+                    </label>
                     <h2 id="text_field">Text Field</h2>
-                    <mwc-textfield
-                        type="email"
-                        label="Email"
-                        icon="alternate_email"
-                    />
+                    <md-filled-text-field type="email" label="Email">
+                        <md-icon slot="leading-icon">alternate_email</md-icon>
+                    </md-filled-text-field>
                     <h2 id="text_area">Text Area</h2>
-                    <mwc-textarea label="Detail" required />
+                    <md-filled-text-field
+                        type="textarea"
+                        label="Detail"
+                        required
+                    />
                     <h2 id="list">List</h2>
-                    <mwc-list activatable multi>
-                        <mwc-list-item>Item 0</mwc-list-item>
-                        <mwc-list-item selected activated>
-                            Item 1
-                        </mwc-list-item>
-                        <mwc-list-item>Item 2</mwc-list-item>
-                        <mwc-list-item selected activated>
-                            Item 3
-                        </mwc-list-item>
-                    </mwc-list>
+                    <md-list>
+                        <md-list-item>Item 0</md-list-item>
+                        <md-list-item>Item 1</md-list-item>
+                        <md-list-item>Item 2</md-list-item>
+                        <md-list-item>Item 3</md-list-item>
+                    </md-list>
                     <h2 id="select">Select</h2>
-                    <mwc-select outlined label="outlined">
-                        <mwc-list-item />
-                        <mwc-list-item value="0">Item 0</mwc-list-item>
-                        <mwc-list-item value="1">Item 1</mwc-list-item>
-                        <mwc-list-item value="2">Item 2</mwc-list-item>
-                        <mwc-list-item value="3">Item 3</mwc-list-item>
-                    </mwc-select>
+                    <md-outlined-select label="outlined">
+                        <md-select-option />
+                        <md-select-option value="0">Item 0</md-select-option>
+                        <md-select-option value="1">Item 1</md-select-option>
+                        <md-select-option value="2">Item 2</md-select-option>
+                        <md-select-option value="3">Item 3</md-select-option>
+                    </md-outlined-select>
                     <h2 id="menu">Menu</h2>
                     <div style={{ position: 'relative' }}>
-                        <mwc-button
-                            raised
-                            onClick={({ target }) =>
-                                (((target as HTMLElement)
-                                    .nextElementSibling as Menu).open = true)
-                            }
-                        >
+                        <md-filled-button id="menu-trigger">
                             Open Menu
-                        </mwc-button>
-                        <mwc-menu id="menu">
-                            <mwc-list-item>Item 0</mwc-list-item>
-                            <mwc-list-item>Item 1</mwc-list-item>
-                            <mwc-list-item>Item 2</mwc-list-item>
-                            <mwc-list-item>Item 3</mwc-list-item>
-                        </mwc-menu>
+                        </md-filled-button>
+                        <md-menu id="menu" anchor="menu-trigger">
+                            <md-menu-item>
+                                <div slot="headline">Item 0</div>
+                            </md-menu-item>
+                            <md-menu-item>
+                                <div slot="headline">Item 1</div>
+                            </md-menu-item>
+                            <md-menu-item>
+                                <div slot="headline">Item 2</div>
+                            </md-menu-item>
+                            <md-menu-item>
+                                <div slot="headline">Item 3</div>
+                            </md-menu-item>
+                        </md-menu>
                     </div>
                     <h2 id="slider">Slider</h2>
-                    <mwc-slider pin markers step={5} max={50} value={10} />
+                    <md-slider ticks step={5} max={50} value={10} />
                     <h2 id="linear_progress">Linear Progress</h2>
-                    <mwc-linear-progress indeterminate />
+                    <md-linear-progress indeterminate />
                     <h2 id="circular_progress">Circular Progress</h2>
-                    <mwc-circular-progress indeterminate />
+                    <md-circular-progress indeterminate />
                     <h2 id="circular_progress_4_colors">
                         Circular Progress - 4 colors
                     </h2>
-                    <mwc-circular-progress-four-color indeterminate />
+                    <md-circular-progress indeterminate />
                     <h2 id="tab_bar">Tab Bar</h2>
-                    <mwc-tab-bar>
-                        <mwc-tab label="Tab one" icon="accessibility" />
-                        <mwc-tab label="Tab two" icon="exit_to_app" />
-                        <mwc-tab label="Tab three" icon="camera" />
-                    </mwc-tab-bar>
+                    <md-tabs>
+                        <md-primary-tab>
+                            <md-icon slot="icon">accessibility</md-icon>Tab one
+                        </md-primary-tab>
+                        <md-primary-tab>
+                            <md-icon slot="icon">exit_to_app</md-icon>Tab two
+                        </md-primary-tab>
+                        <md-primary-tab>
+                            <md-icon slot="icon">camera</md-icon>Tab three
+                        </md-primary-tab>
+                    </md-tabs>
                     <h2 id="snack_bar">Snack Bar</h2>
-                    <mwc-button
-                        raised
+                    <md-filled-button
                         onClick={({ target }) =>
-                            ((target as HTMLElement)
-                                .nextElementSibling as Snackbar).show()
+                            (
+                                (target as HTMLElement)
+                                    .nextElementSibling as Snackbar
+                            ).show()
                         }
                     >
                         Show Snackbar
-                    </mwc-button>
-                    <mwc-snackbar labelText="Can't send photo. Retry in 5 seconds.">
-                        <mwc-button slot="action">RETRY</mwc-button>
-                    </mwc-snackbar>
+                    </md-filled-button>
+                    <md-snackbar labelText="Can't send photo. Retry in 5 seconds.">
+                        <md-filled-button slot="action">RETRY</md-filled-button>
+                    </md-snackbar>
                     <h2 id="dialog">Dialog</h2>
-                    <mwc-button
-                        raised
+                    <md-filled-button
                         onClick={({ target }) =>
-                            (((target as HTMLElement)
-                                .nextElementSibling as Dialog).open = true)
+                            (
+                                (target as HTMLElement)
+                                    .nextElementSibling as MdDialog
+                            ).show()
                         }
                     >
                         Show Alert
-                    </mwc-button>
-                    <mwc-dialog>
-                        Discard draft?
-                        <mwc-button slot="primaryAction" dialogAction="discard">
-                            Discard
-                        </mwc-button>
-                        <mwc-button
-                            slot="secondaryAction"
-                            dialogAction="cancel"
-                        >
-                            Cancel
-                        </mwc-button>
-                    </mwc-dialog>
+                    </md-filled-button>
+                    <md-dialog>
+                        <div slot="headline">Discard draft?</div>
+                        {/* @ts-ignore */}
+                        <form slot="content" id="form-id" method="dialog" />
+                        <div slot="actions">
+                            <md-text-button value="discard">
+                                Discard
+                            </md-text-button>
+                            <md-text-button value="cancel">
+                                Cancel
+                            </md-text-button>
+                        </div>
+                    </md-dialog>
                 </main>
-            </mwc-top-app-bar-fixed>
-        </mwc-drawer>
+            </md-top-app-bar-fixed>
+        </md-drawer>
     )
 );
